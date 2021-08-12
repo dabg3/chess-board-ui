3148702aeae9f8d1ebbf0add8faaa124d28f4cab
dav98b <bigotti.davide@hotmail.com>
2021-08-12 11:08:48 +0200

    ngx-svg rendering draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,24 +18,24 @@ export class AppComponent implements OnInit, AfterViewInit {
 	constructor() { }
 
 	ngOnInit(): void {
-		const canvas: HTMLCanvasElement | null = this.canvasRef.nativeElement;
-		if (!canvas) {
-			throw new Error('unable to retrieve HtmlCanvasElement instance');
-		}
-		if (!(canvas.height == canvas.width)) {
-			throw new Error('canvas is not squared');
-		}
-		const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
-		if (!ctx) {
-			throw new Error('unable to retrieve CanvasRenderingContext2D instance');
-		}
-		this.canvas = canvas;
-		this._2dContext = ctx;
+		// const canvas: HTMLCanvasElement | null = this.canvasRef.nativeElement;
+		// if (!canvas) {
+		// 	throw new Error('unable to retrieve HtmlCanvasElement instance');
+		// }
+		// if (!(canvas.height == canvas.width)) {
+		// 	throw new Error('canvas is not squared');
+		// }
+		// const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
+		// if (!ctx) {
+		// 	throw new Error('unable to retrieve CanvasRenderingContext2D instance');
+		// }
+		// this.canvas = canvas;
+		// this._2dContext = ctx;
 	}
 
 	ngAfterViewInit(): void {
-		this.squareSizePx = this.canvas.height / 8;
-		this.drawChessSquares();
+		// this.squareSizePx = this.canvas.height / 8;
+		// this.drawChessSquares();
 	}
 
 	private drawChessSquares(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+
+import { NgxSvgModule } from 'ngx-svg';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxSvgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
