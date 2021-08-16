12b542b25a137409cfdc4bf2a07d983c0eb0f4f0
dav98b <bigotti.davide@hotmail.com>
2021-08-16 15:58:40 +0200

    move piece rendering to component

--- src/app/board/piece-set/piece-set.module.ts
+++ src/app/board/piece-set/piece-set.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxSvgModule } from 'ngx-svg';
+import { PieceComponent } from './piece/piece.component';
 
 
 
 @NgModule({
 	declarations: [
+		PieceComponent
 	],
 	imports: [
 		CommonModule,
 		NgxSvgModule
 	],
 	exports: [
+		PieceComponent
 	]
 })
 export class PieceSetModule { }
---
+++ src/app/board/piece-set/piece/piece.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Square } from '../../square-mapping/square';
+
+@Component({
+	selector: 'app-piece',
+	templateUrl: './piece.component.html',
+	styleUrls: ['./piece.component.css']
+})
+export class PieceComponent implements OnInit {
+
+	@Input('square') square: Square;
+	@Input('squareSizePx') sqPx: number; 
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+}
